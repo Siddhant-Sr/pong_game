78150ab0d5f0ca2340ab7c50eff318f1615e27d7
Siddhant <siddhantsrivastava0811@gmail.com>
2020-12-31 11:03:41 +0530

    Pong game completed

---
+++ paddle.py
@@ -0,0 +1,23 @@
+from turtle import  Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.goto(position)
+        self.speed("fastest")
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
